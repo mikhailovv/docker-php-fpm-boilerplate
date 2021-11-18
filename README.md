28e36a5d13686c5522f869ad44ce5df3d4b76b7d
mikhailovv <vmikhailov2015@gmail.com>
2021-11-18 22:49:51 +0300

    update php and postgres

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # docker-php-fpm-boilerplate
-Simple boilerplate for start local develop with NGINX, Postrgres 12, PHP 7.4, Xdebug
+Simple boilerplate for start local develop with NGINX, Postrgres 14, PHP 8.1, Xdebug
 
 ## Current configuration
-- PHP 7.4RC06
+- PHP 8.1RC06
 - Xdebug
-- Postgres 12
+- Postgres 14
 - Nginx
 
 ## Required
--- public/index.php
+++ public/index.php
@@ -6,7 +6,40 @@ $y = 1;
 
 $summ = fn($x) => fn($z) => $x + $z;
 
-assert(9, $summ(5)(4));
-assert( 15, $summ(5)($summ(4)(6)));
+assert(9 == $summ(5)(4));
+assert(15 == $summ(5)($summ(4)(6)));
 
-echo 'all Ok';
+echo 'all Ok<br/>';
+
+enum EntityStatus: string
+{
+    case CREATED = 'created';
+    case READY = 'ready';
+    case PROCESSING = 'processing';
+    case COMPLETED = 'completed';
+}
+
+
+class Entity
+{
+    public function __construct(public readonly int $id, public EntityStatus $status)
+    {
+    }
+}
+
+echo sprintf('Before cycle: %s <br/>', memoryUsage());
+
+for ($i = 0; $i < 10000; $i++) {
+    $entity = new Entity($i, EntityStatus::cases()[array_rand(EntityStatus::cases())]);
+}
+
+echo sprintf('After cycle: %s <br/>', memoryUsage());
+
+function memoryUsage(): string
+{
+    return sprintf(
+        'Memory usage: <strong>%dKB</strong> Peak usage: <strong>%dKB</strong>',
+        round(memory_get_usage() / 1024),
+        round(memory_get_peak_usage() / 1024)
+    );
+}
